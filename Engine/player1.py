c51336f2463f024b04705034b045f008d75d37ee
Farhan Daemi <fdmxfarhan@yahoo.com>
2022-02-16 14:59:52 +0330

    asdf

--- Engine/player1.py
+++ Engine/player1.py
@@ -1,11 +1,21 @@
 import sys
-init_msg = input().split(' ')
+a = input()
 print('init confirm')
 
 while True:
-    state_msg = input()
-    print(state_msg, file=sys.stderr)
-    print(1)
+    msg = input().split(' ')
+    step = int(msg[0])
+    x = int(msg[2])
+    y = int(msg[3])
+    enemyInVision = int(msg[8])
 
+    if(enemyInVision == 0):
+        numOfTiles = int(msg[9])
+        for i in range(numOfTiles):
+            x1 = int(msg[i*3])
+            y1 = int(msg[i*3+1])
+            state = int(msg[i*3+2])
+            print(x1, y1, state, file=sys.stderr)
 
+    print(4)
     
